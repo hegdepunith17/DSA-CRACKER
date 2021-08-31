f0b873f8d7dfbfea32d1ea8df4790c16c062580b
Punith B Hegde <58084673+hegdepunith17@users.noreply.github.com>
2021-08-31 09:44:19 +0530

    Create #473 Find the two non-repeating elements in an array of repeating elements.cpp

---
+++ #473 Find the two non-repeating elements in an array of repeating elements.cpp
@@ -0,0 +1,12 @@
+  vector<int> singleNumber(vector<int> nums) 
+    {
+        int xr=0,res1,res2;
+        for(int i=0;i<nums.size();i++){
+            xr=xr^nums[i];
+            
+        }
+        int sn=xr&~(xr-1);
+        for(int i=0;i<n;i++){
+            if(nums[i]&)
+        }
+    }
