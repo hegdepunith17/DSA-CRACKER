35c6c6a94da17fbe5f7f77c66fcc3b6ae195b59c
Punith B Hegde <58084673+hegdepunith17@users.noreply.github.com>
2021-09-22 11:49:21 +0530

    Create Majority Element.cpp

---
+++ Majority Element.cpp
@@ -0,0 +1,22 @@
+int majorityElement(int a[], int size)
+{
+    int ans=a[0];
+    int count =1;
+    for(int i=1;i<size;i++)
+    {
+        if (a[i]==ans) count ++;
+        else count --;
+        if (count == 0)
+        {
+            ans=a[i];
+            count =1;
+            
+        }
+    }
+    count =0;
+    for(int i=0;i<size;i++)
+    {
+        if (a[i]==ans) count ++;
+    }
+    if(count >size/2) return ans ;
+    else  return -1;
