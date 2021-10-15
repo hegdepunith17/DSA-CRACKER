88742a1b113aedc8edf1399de63f4b656094f624
Punith B Hegde <58084673+hegdepunith17@users.noreply.github.com>
2021-10-15 11:04:53 +0530

    Update 706. Design HashMap.cpp

--- 706. Design HashMap.cpp
+++ 706. Design HashMap.cpp
@@ -21,3 +21,52 @@ public:
         m[key]=-1;
     }
 };
+class MyHashMap {
+public:
+    vector<list<pair<int,int >>>m;
+    int size;
+    
+    MyHashMap() {
+        size=1100;
+        m.resize(size);
+        
+    }
+    int hash(int key)
+    {
+        return key%size;
+        
+    }
+    list<pair<int,int >>::iterator search (int key)
+    {
+        int i=hash(key);
+        list<pair<int,int >>::iterator it=m[i].begin();
+       while(it!=m[i].end()){
+            if(it->first==key)return it;
+        it++;
+        }
+        return it;
+    }
+    
+    void put(int key, int value) {
+       int i=hash(key);
+        remove(key);
+        m[i].push_back({key,value});
+    }
+    
+    int get(int key) {
+        int i=hash(key);
+        list<pair<int,int >>::iterator it=search(key);
+        if(it==m[i].end())return -1;
+        else return it->second;
+        
+    }
+    
+    void remove(int key) {
+        int i=hash(key);
+       list<pair<int,int >>::iterator it=search(key);
+        if(it!=m[i].end())m[i].erase(it);
+        
+        
+        
+    }
+};
